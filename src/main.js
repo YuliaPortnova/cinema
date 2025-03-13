4ac0b52fd8ba99eeb4348940d63eb45727ebcce9
Yulia Portnova <yuyul@yandex.ru>
2025-03-13 09:55:11 +0300

    feat: разбивает код на модули

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import HeaderProfileView from './view/header-profile-view.js';
+import FooterStatisticsView from './view/footer-statistics-view.js';
+import FilterView from './view/filter-view.js';
+
 import { render } from './render.js';
 
 const bodyElement = document.querySelector('body');
 const siteHeaderElement = bodyElement.querySelector('.header');
+const siteMainElement = bodyElement.querySelector('.main');
+const siteFooterElement = bodyElement.querySelector('.footer');
+const footerStatisticsElement = siteFooterElement.querySelector('.footer__statistics');
 
 render(new HeaderProfileView(), siteHeaderElement);
+render(new FilterView(), siteMainElement);
+render(new FooterStatisticsView(), footerStatisticsElement);
---
+++ src/view/film-details-view.js
@@ -0,0 +1,57 @@
+import { createElement } from '../render.js';
+import { createFilmDetailsInfoTemplate } from './film-details-info-template.js';
+import { createFilmDetailsControlsTemplate } from './film-details-controls-template.js';
+import { createFilmDetailsCommentsTemplate } from './film-details-comments-template.js';
+import { createFilmDetailsFormTemplate } from './film-details-form-template.js';
+
+const createFilmDetailsTemplate = () =>
+  `
+    <section class="film-details">
+      <form class="film-details__inner" action="" method="get">
+        <div class="film-details__top-container">
+          <div class="film-details__close">
+            <button class="film-details__close-btn" type="button">close</button>
+          </div>
+          <div class="film-details__info-wrap">
+            <div class="film-details__poster">
+              <img class="film-details__poster-img" src="./images/posters/the-great-flamarion.jpg" alt="">
+
+              <p class="film-details__age">18+</p>
+            </div>
+
+            ${createFilmDetailsInfoTemplate()}
+          </div>
+
+          ${createFilmDetailsControlsTemplate()}
+        </div>
+
+        <div class="film-details__bottom-container">
+          <section class="film-details__comments-wrap">
+            <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">4</span></h3>
+
+            ${createFilmDetailsCommentsTemplate()}
+
+            ${createFilmDetailsFormTemplate()}
+          </section>
+        </div>
+      </form>
+    </section>
+  `;
+
+export default class FilmDetailsView {
+  getTemplate() {
+    return createFilmDetailsTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
