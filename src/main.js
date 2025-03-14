b047bec3941bcaa6d65a4b66b8ab056db2d3ffb8
Yulia Portnova <yuyul@yandex.ru>
2025-03-14 08:50:22 +0300

    feat: добавляет презентор фильмов

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import HeaderProfileView from './view/header-profile-view.js';
 import FooterStatisticsView from './view/footer-statistics-view.js';
 import FilterView from './view/filter-view.js';
+import FilmsPresenter from './presenter/films-presenter.js';
 
 import { render } from './render.js';
 
@@ -10,6 +11,10 @@ const siteMainElement = bodyElement.querySelector('.main');
 const siteFooterElement = bodyElement.querySelector('.footer');
 const footerStatisticsElement = siteFooterElement.querySelector('.footer__statistics');
 
+const filmsPresenter = new FilmsPresenter();
+
 render(new HeaderProfileView(), siteHeaderElement);
 render(new FilterView(), siteMainElement);
 render(new FooterStatisticsView(), footerStatisticsElement);
+
+filmsPresenter.init(siteMainElement);
---
+++ src/presenter/films-presenter.js
@@ -0,0 +1,33 @@
+import FilmButtonMoreView from '../view/film-button-more-view.js';
+import FilmCardView from '../view/film-card-view.js';
+import FilmDetailsView from '../view/film-details-view.js';
+import FilmsListContainerView from '../view/films-list-container-view.js';
+import FilmsListView from '../view/films-list-view.js';
+import FilmsView from '../view/films-view.js';
+import SortView from '../view/sort-view.js';
+
+import { render } from '../render.js';
+
+export default class FilmsPresenter {
+  filmButtonMoreComponent = new FilmButtonMoreView();
+  filmsListContainerComponent = new FilmsListContainerView();
+  filmsListComponent = new FilmsListView();
+  filmsComponent = new FilmsView();
+  sortComponent = new SortView();
+
+  init(container) {
+    this.container = container;
+
+    render(this.sortComponent, this.container);
+    render(this.filmsComponent, this.container);
+    render(this.filmsListComponent, this.filmsComponent.getElement());
+    render(this.filmsListContainerComponent, this.filmsListComponent.getElement());
+
+    for (let i = 0; i < 5; i++) {
+      render(new FilmCardView(), this.filmsListContainerComponent.getElement());
+    }
+
+    render(this.filmButtonMoreComponent, this.filmsListComponent.getElement());
+    render(new FilmDetailsView(), this.container.parentElement);
+  }
+}
